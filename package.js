288ce23d3a735b4eecc6ed32827996a1fa9f6f51
rh80114477-netizen <rh80114477@gmail.com>
2026-08-29 23:37:46 +0600

    Initialize package.json for backend API project

--- package.js
+++ package.js
@@ -1,1 +1,26 @@
-
+{
+  "name": "teleshort-mini-app-api",
+  "version": "1.0.0",
+  "description": "Backend API for TeleShort Telegram Mini App Earning System",
+  "main": "index.js",
+  "scripts": {
+    "start": "echo \"This project is designed for Vercel Serverless Functions.\" && exit 0",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "telegram",
+    "mini-app",
+    "url-shortener",
+    "bot",
+    "vercel",
+    "supabase"
+  ],
+  "author": "WebApp Sikho",
+  "license": "ISC",
+  "dependencies": {
+    "@supabase/supabase-js": "^2.39.0"
+  },
+  "engines": {
+    "node": ">=18.x"
+  }
+}
